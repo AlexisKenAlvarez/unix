c600ab11edce5bcf16f7b489083c2a45fad2c178
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-09-19 22:33:40 +0800

    logo loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -8,7 +8,7 @@ const Loader = () => {
   return (
     <>
         <motion.div exit={{opacity: 0, transition: {duration: 0.5}}} className='loader-container'>
-            <motion.img initial={{scale: 2, opacity: 0}} animate={{opacity: 1,scale: 1, transition: {delay: 0.2, duration: 0.5}}} exit={{opacity: 0, transition: {duration: 0.5}}} src="https://unix-shop.s3.ap-southeast-1.amazonaws.com/logowhite.png" className='loader-image'></motion.img>
+            <motion.img initial={{scale: 2, opacity: 0}} animate={{opacity: 1,scale: 1, transition: {delay: 0.2, duration: 0.5}}} exit={{opacity: 0, transition: {duration: 1}}} src="https://unix-shop.s3.ap-southeast-1.amazonaws.com/logowhite.png" className='loader-image'></motion.img>
         </motion.div>
     </>
   )
