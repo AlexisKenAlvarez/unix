b4010d1eb2103b5cb740ce9f4212ce923b89dd59
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-09-19 22:48:54 +0800

    fixed loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -9,7 +9,7 @@ const Loader = () => {
     <>
         <div className='loader-wrapper'>
             <motion.div exit={{opacity: 0, transition: {duration: 0.5}}} className='loader-container'></motion.div>
-            <motion.img initial={{scale: 2, opacity: 0}} animate={{opacity: 1,scale: 1, transition: {delay: 0.2, duration: 0.5}}} exit={{opacity: 0, transition: {duration: 1.5}}} src="https://unix-shop.s3.ap-southeast-1.amazonaws.com/logowhite.png" className='loader-image'></motion.img>
+            <motion.img initial={{scale: 2, opacity: 0}} animate={{opacity: 1,scale: 1, transition: {delay: 0.2, duration: 0.5}}} exit={{opacity: 0, transition: {duration: 1}}} src="https://unix-shop.s3.ap-southeast-1.amazonaws.com/logowhite.png" className='loader-image'></motion.img>
         </div>
     </>
   )
